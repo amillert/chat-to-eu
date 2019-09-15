cfab53c367b0576cd3a95bd7a633e0afa472f773
devmood <amillert@roq.ad>
2019-09-15 04:00:25 +0200

    Fixes in topic modeling

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from topic_modeling.modeling import Modeler
 if __name__ == "__main__":
     tmp_path = os.path.join(os.getcwd(), "model/")
     if len(os.listdir(tmp_path)) > 0:
-        print(1)
-        exit(1)
-        model = Modeler(tmp_path)
+        modeler = Modeler(tmp_path)
     else:
         preprocessor = Preprocessor()
-        model = Modeler(tmp_path, preprocessor.paras_tokenized, False)
-    
-    print("Ukończone!")
-    exit(12)
+        modeler = Modeler(tmp_path, preprocessor.paras_tokenized, False)
+    model = modeler.lda_model
